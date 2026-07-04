c4eebd3412fb6b6cd89260b2eca8386cf409cc91
Anish Singh <anishsingh.70455@gmail.com>
2026-07-04 23:38:30 +0530

    Time: 9 ms (62.02%), Space: 21 MB (72.98%) - LeetHub

---
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def sortedSquares(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        left = 0
+        right = n - 1
+        result = [0] * n
+
+        for i in range(n-1,-1,-1):
+            if abs(nums[left]) > abs(nums[right]):
+                result[i] = nums[left] ** 2
+                left += 1
+            else:
+                result[i] = nums[right] ** 2 
+                right -= 1
+        return result            
